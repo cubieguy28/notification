97c481f453de7b7d5abee17fec17568e775eb0ed
Ivan <ivanminez28@gmail.com>
2021-03-01 19:34:29 +0800

    Initial commit

---
+++ app/src/main/java/com/example/reminderapp/RoomDAO.java
@@ -0,0 +1,33 @@
+package com.example.reminderapp;
+
+
+import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Delete;
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.Query;
+import android.arch.persistence.room.Update;
+
+import java.util.List;
+
+@Dao
+public interface RoomDAO {
+
+    @Insert
+    public void Insert(Reminders... reminders);
+
+    @Update
+    public void Update(Reminders... reminders);
+
+    @Delete
+    public void Delete(Reminders reminders);
+
+    @Query("Select * from reminder order by remindDate")
+    public List<Reminders> orderThetable();
+
+    @Query("Select * from reminder Limit 1")
+    public Reminders getRecentEnteredData();
+
+    @Query("Select * from reminder")
+    public List<Reminders> getAll();
+
+}
